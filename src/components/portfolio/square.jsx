a56353f7c6b99a7dacbfe4df00a9f5a06757b8b7
posy-m <minji.posy@gmail.com>
2024-12-05 04:56:59 +0900

    portfolio css 수정

--- src/components/portfolio/square.jsx
+++ src/components/portfolio/square.jsx
@@ -2,16 +2,21 @@ import React from 'react'
 import styles from './square.module.css'
 
 function Square({ date, title, content, src, type, alt, onClick }) {
-  const imgsize = type === 'web' ? styles.webSize : styles.phoneSize;
+  // const imgsize = type === 'web' ? styles.webSize : styles.phoneSize;
   return (
-
-    <div className={styles.squareBox} onClick={onClick}>
-      <div className={styles.squareTextBox}>
-        <span>{title}</span>
-        <span>{date}</span>
-        <span>{content}</span>
+    <div className={styles.squareAll}>
+      <div className={styles.projectnNo}>project</div>
+      <div className={styles.squareBox} onClick={onClick}>
+        <div className={styles.squareTextBox}>
+          <span>{title}</span>
+          <span>{date}</span>
+          <span>{content}</span>
+          <div className={styles.imgSize}>
+            <img src={src} alt={alt} className={styles.imgContent} />
+          </div>
+        </div>
+        {/* <img src={src} alt={alt} className={imgsize} /> */}
       </div>
-      <img src={src} alt={alt} className={imgsize} />
     </div>
 
   )
